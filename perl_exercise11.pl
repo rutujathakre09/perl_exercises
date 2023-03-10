874b888b0de78f07663744ef6fbc72efe6eff7c5
device.develper <devicedeveloper@biztransights.com>
2023-03-10 12:46:23 +0530

    exercises to review 10 march

---
+++ perl_exercise11.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+=head
+ Input a line of words. Print statistics of words in the line
+=cut 
+
+print "Enter a sentense:";
+chomp(my $line = <STDIN>);
+my @words = split(" ",$line);
+my $count = 0;
+foreach my $word (@words)
+{
+	my $length_of_word = length($word);
+	print "Number of charecters in $word : $length_of_word\n";
+	$count++;
+}
+print "Total Number of words in sentense : $count\n";
+
+
+
+
+
+
---
+++ perl_exercise12.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+=head
+ Input transaction type (sales, purchase, credit) and qty, till transaction type is 'end'.
+ Calculate and print total sales, purchase and credit.
+=cut 
+
+my %transaction_details;
+my $type;
+my $quantity;
+
+while(1){
+print "Enter a transaction type:";
+chomp($type = <STDIN>);
+last if($type eq "end");
+print "Enter quantity:";
+chomp($quantity = <STDIN>);
+$transaction_details{$type} += $quantity;
+}
+print("Transaction type total:\n");
+while( my ($k,$v) = each %transaction_details)
+{
+    print("$k:$v\n");
+}
+
+
+
+
+
+
